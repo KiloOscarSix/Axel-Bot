830eb8804831fa294402c4d28ca4b338136bc921
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2022-11-29 11:58:00 +0000

    fixed callback update

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,22 @@
 import http from "http"
 import {Axel} from "../client";
+import {ILovenseConnection} from "../models/lovense";
 
 export default function init(client: Axel) {
     const server = http.createServer((request) => {
-        console.log("Got request!")
-
         if (request.method == "POST") {
             let body = ""
             request.on("data", data => {
                 body += data
             })
             request.on("end", () => {
-                console.log(body)
-                // const jsonContent: ILovenseConnection = JSON.parse(body)
-                // client.connectedUsers.set(jsonContent.uid, jsonContent)
+                const jsonContent: ILovenseConnection = JSON.parse(body)
+
+                client.connectedUsers.set(jsonContent.uid, jsonContent)
+
+                if (!client.connectedUsers.has(jsonContent.uid)) {
+                    console.log("New User Connected")
+                }
             })
         }
     })
